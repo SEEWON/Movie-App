a7f75040242d1b7c6999db2f6febbfbd287071f3
SEEWON <andyaaa78@gmail.com>
2020-08-17 16:14:28 +0900

    2 files modified, 8 added, 2 deleted

---
+++ src/routes/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>
+      This page is made by SEEWON, beginner wep-app developer in 2020-08-17.
+      </span>
+      <span>
+      ✉Mail me✉ : andyaaa78@Gmail dot com
+      </span>
+    </div>
+  );
+}
+
+export default About;
